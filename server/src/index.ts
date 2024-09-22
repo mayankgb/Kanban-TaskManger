00e34fcd3b225efd4f865d9ebea524addc6ae632
MAYANK AGRAWAL <mayk03jun@gmail.com>
2024-09-22 11:45:08 +0530

    changing the cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://kanban-task-manger-chi.vercel.app",
     credentials:true
 }))
 
